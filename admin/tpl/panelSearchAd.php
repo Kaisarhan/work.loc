4b8cb9d6a1db875f86ca3552896073b91ceefc55
Kaisarhan <kaisarhan@mail.ru>
2021-03-28 02:06:56 +0600

    исправлен баг с выходам из админ в пойске

--- admin/tpl/panelSearchAd.php
+++ admin/tpl/panelSearchAd.php
@@ -3,7 +3,7 @@
     <h1 class="Title_name">Журнал для ведения учета USB</h1>
     <div class="container form_buttons">
         <div class="add_del">
-            <a class="btn btn-outline-danger" href="../adrc/logOut.php" role="button" title="Выйти из Админ панели"><i class="bi bi-box-arrow-left"></i></a>
+            <a class="btn btn-outline-danger" href="../../adrc/logOut.php" role="button" title="Выйти из Админ панели"><i class="bi bi-box-arrow-left"></i></a>
             <a class="btn btn-primary" href="/" role="button" title="Гостевая панель"><i class="bi bi-person-lines-fill"></i></a>
             <a class="btn btn-success" href="/admin" role="button" title="Полный список"><i class="bi bi-file-text"></i></a>
             <button type="button" class="btn btn-success" id="myInput" data-bs-toggle="modal" data-bs-target="#myModal" data-bs-whatever="@mdo" title="Добавить новую запись"><i class="bi bi-clipboard-plus"></i></button>
--- tpl/panelSearch.php
+++ tpl/panelSearch.php
@@ -2,14 +2,8 @@
     <h1 class="Title_name">Журнал для ведения учета USB</h1>
     <div class="container form_buttons">
         <div class="add_del">
-            <a class="btn btn-primary" href="/admin" role="button">Admin Panel</a>
-        </div>
-        <div class="search">
-            <form class="d-flex" action="search.php" id="search">
-                <input class="form-control me-2" name="searchInput" type="search" placeholder="Найти" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Найти</button>
-            <button type="submit" onclick="window.location.href='/'" class="btn btn-success">Полный список</button>
-            <a class="btn btn-primary" href="/admin" role="button">Admin Panel</a>
+            <a class="btn btn-primary" href="/admin" role="button" title="Админ панель"><i class="bi bi-emoji-laughing"></i></a>
+            <a class="btn btn-success" href="/" role="button" title="Полный список"><i class="bi bi-file-text"></i></a>
         </div>
         <div class="search">
             <form class="d-flex" method="get" action="search.php" id="search">
