88ea08c343c878ba87ebac353403a9f193951557
Kaisarhan <kaisarhan@mail.ru>
2021-03-27 20:25:55 +0600

    Настройка системы пойска пойска

---
+++ tpl/panelSearch.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!-- buttons add, edit, search -->
+<section class="form_add_elemets">
+    <h1 class="Title_name">Журнал для ведения учета USB</h1>
+    <div class="container form_buttons">
+        <div class="add_del">
+            <button type="submit" onclick="window.location.href='/'" class="btn btn-success">Полный список</button>
+            <a class="btn btn-primary" href="/admin" role="button">Admin Panel</a>
+        </div>
+        <div class="search">
+            <form class="d-flex" method="get" action="search.php" id="search">
+                <input class="form-control me-2" type="search" name="searchInput" placeholder="Пойск..." aria-label="Search">
+                <button type="submit" class="btn btn-outline-success">Найти</button>
+            </form>
+        </div>
+    </div>
+</section>
+
